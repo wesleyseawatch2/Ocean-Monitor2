947fc548d2d2307220db973d2fb62a758972ba9d
wesleyseawatch2 <wesleyseawatch2@gmail.com>
2025-12-02 16:15:58 +0800

    1202 5 commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -7,11 +7,8 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-SECRET_KEY = 'django-insecure-your-secret-key-here'
+SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
